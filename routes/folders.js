f4c760ed469a67d73cc4c514789741a82aed0d61
Shane Lupton <slupton89@gmail.com>
2018-09-20 13:46:27 -0600

    Added folders model, seeddb, and routes

---
+++ routes/folders.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Folder = require('../models/folder');
+
+const router = express.Router();
+
+
+// GET
+
+router.get('/', (req, res, next) => {
+  const { searchTerm } = req.query;
+  console.log('1');
+  let filter = {};
+
+  if (searchTerm) {
+    filter.name = { $regex: searchTerm, $options: 'i' };
+  }
+
+  Folder.find(filter)
+    .sort({ updatedAt: 'desc' })
+    .then(results => {
+
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Folder.findById(id)
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+// POST
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/', (req, res, next) => {
+  const { name } = req.body;
+
+  /***** Never trust users - validate input *****/
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newFolder = { name };
+
+  Folder.create(newFolder)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+// PUT
+
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateFolder = { name };
+
+  Folder.findByIdAndUpdate(id, updateFolder, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+// DELETE
+
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Folder.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+
+
+
+
+
+
+module.exports = router;
--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
 const { notes } = require('../db/seed/notes');
+const { folders } = require('../db/seed/folders');
 
 console.info('Connecting to:', MONGODB_URI);
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
@@ -15,7 +17,11 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   })
   .then(() => {
     console.info('Seeding Database');
-    return Note.insertMany(notes);
+    return Promise.all([
+      Note.insertMany(notes),
+      Folder.insertMany(folders),
+      Folder.createIndexes(),
+    ]);
   })
   .then(() => {
     console.info('Disconnecting');
